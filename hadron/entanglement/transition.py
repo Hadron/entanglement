59bd82cf186d86f402426ce148125402c66b974a
Sam Hartman <sam.hartman@hadronindustries.com>
2017-07-22 12:25:52 -0400

    Document security and correctness requirements for transitions

--- hadron/entanglement/transition.py
+++ hadron/entanglement/transition.py
@@ -18,7 +18,67 @@ transition_objects = weakref.WeakKeyDictionary()
 
 class TransitionTrackerMixin (interface.Synchronizable):
 
-    "Tracks entanglement Synchronizable objects for fast moving changes like transitions"
+    '''Add support for transitions.  Transitions are intended to notify
+    SyncManagers about fast-moving operations like moving graphical
+    elements.
+
+    Transitions are intended to inform other SyncManagers of a possible
+    change as it happens.  When an object exits transition, all parties
+    are required to restore state as if the transition had not happened.
+    If the transitioning SyncManager wishes to propose the final state of
+    the transition as permanent, a sync or forward needs to be
+    synchronized.
+
+    This class is typically used by a Mixin that adapts transition
+    tracking to a particular persistence technology.  The method
+    resolution order typically needs to be TransitionTrackerMixin's
+    sync_construct, to look for objects in transition, followed by the
+    persistence layer's sync_construct, followed by Synchronizable's
+    sync_construct for totally new objects.  The easiest way to
+    achieve that is to have a persistence layer Mixin.  For example,
+    SqlTransitionTrackerMixin is declared as follows:
+
+        class SqlTransitionTrackerMixin(TransitionTrackerMixin, SqlSynchronizable):
+
+    It would be somewhat tricky for a persistence layer to be entirely
+    wrapping TransitionTrackerMixin's sync_construct because the
+    wrapping code would need to distinguish returns from
+    TransitionTrackerMixin to see whether they were objects in
+    transition or whether they were new objects that might erroneously
+    shadow persisted objects.
+
+    Not all the nodes that get to review and approve forwards and
+    syncs may process transitions.  As a result, objects in transition
+    may not be as trustworthy as other objects.  To maintain security,
+    parties other than the SyncManager initiating a transition SHOULD
+    NOT use a transitioned object as a basis for sync or forward
+    operations.  If a transitioned object is used as a basis for sync
+    or forward, all changes between the most recent synchronized
+    version and the transitioned object MUST be evaluated and approved
+    by the party making the update.  The simplest way of guaranteeing
+    this is to override remove_from_transition and make sure that when
+    remove_from_transition is called, future lookups and uses of the
+    object get the most recently persisted version.  However, the
+    instance on which remove_from_transition is called needs to remain
+    with the transitioned values so that the following pattern works
+    for the party initiating transition:
+
+        while moving: 
+            # updates values on obj
+            obj.perform_transition()
+        # We like the final state
+        obj.remove_from_transition()
+        persistence_layer.save(obj) #forwards or syncs
+
+    Along the same lines, subclasses MUST guarantee that when
+    TransitionTrackerMixin.sync_construct calls remove_from_transition
+    and then later calls the superclass sync_construct, that the most
+    recently persisted object is returned from the superclass rather
+    than the object under transition. SqlTransitionTrackerMixin meets
+    this requirement.
+
+    '''
+
 
     class transition_tracked_objects:
         def __get__(self, inst, cls):
@@ -98,4 +158,3 @@ class TransitionTrackerMixin (interface.Synchronizable):
     def transition_modified_attrs(self):
         "Override to return the set of attributes that have been modified and should be included in the transition message."
         return None
-    
