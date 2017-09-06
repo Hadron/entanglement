886a681a44b3883f931e6305d7b3565a2d4e3a0a
Sam Hartman <sam.hartman@hadronindustries.com>
2017-09-06 13:23:07 -0400

    Add no_response handling to entanglement.js

--- javascript/entanglement.js
+++ javascript/entanglement.js
@@ -3,6 +3,9 @@
 if (!('WebSocket' in this)) {
     var WebSocket =require('websocket').w3cwebsocket;
 }
+try {
+    var uuid = require('node-uuid');
+} catch(err) {}
 
 class  SyncManager {
     constructor(url) {
@@ -10,6 +13,15 @@ class  SyncManager {
 	this.socket.addEventListener('message', event => {
 	    this._in_counter++;
 	    var message = JSON.parse(event.data);
+	    if ( '_no_resp_for' in message) {
+		message._no_resp_for.forEach( n => {
+		    var ex = this.expected[number(n)];
+		    if (ex) {
+			ex.forEach( p => p.resolved(null));
+			delete this.expected[number(n)];
+		    }
+		});
+	    }
 	    if (!(message._resp_for === undefined)) {
 		message._resp_for.forEach( r => {
 		    if( Number(r) in this.expected) {
@@ -37,6 +49,7 @@ class  SyncManager {
 	var obj2 = {};
 	attributes.forEach( attr => obj2[attr] = obj[attr]);
 	obj2['_sync_operation'] = operation;
+	obj2._sync_owner = obj._sync_owner;
 	if (response === true) {
 	    obj2['_flags'] = 1;
 	    res = new Promise((resolved, rejected) => {
@@ -63,6 +76,26 @@ class  SyncManager {
 	var handlers = this.receivers[type];
 	this.receivers[type] = handlers.filter(h => h === handler);
     }
+
+    perform_transition(obj, attributes) {
+	var first_transition = false;
+	var result;
+	if(obj.transition_id == null ) { // undefined is OK too
+	    obj.transition_id = uuid.v4();
+	    first_transition = true;
+	} else {
+	    result = obj.transition_promise;
+	}
+	var full_attrs = Array.from(attributes);
+	full_attrs.push('transition_id');
+	var sync_result = this.synchronize(obj, full_attrs, 'transition', first_transition);
+    if (first_transition) {
+	obj.transition_promise = sync_result;
+	result = sync_result;
+    }
+    return result;
+}
+
 
     close() { this.socket.close();}
     
