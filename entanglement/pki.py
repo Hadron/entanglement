583807d5222e68398ca5a8602067716850e17bfe
Sam Hartman <sam.hartman@hadronindustries.com>
2018-08-30 09:20:19 -0400

    Add prefix to entanglement-pki
    Add a prefix argument to entanglement-pki so certs with additional OUs
    do not overlap with simple CN certs.

--- entanglement/pki.py
+++ entanglement/pki.py
@@ -26,11 +26,13 @@ def gen_site_ca(pki_dir, ca_name = "Root CA"):
                    '-extensions', 'v3_ca',
                    '-out', ca_pem)
 
-def host_cert(pki_dir, hostname, adl_subj):
+def host_cert(pki_dir, hostname, adl_subj, prefix = None):
+    if prefix is not None:
+        prefix = prefix+"_"
     ca_key = os.path.join(pki_dir, 'ca.key')
     ca_pem = os.path.join(pki_dir, 'ca.pem')
     gen_site_ca(pki_dir)
-    hostfile = os.path.join(pki_dir, hostname)
+    hostfile = os.path.join(pki_dir, prefix+hostname)
     
     if not (exists (hostfile + '.key') and exists(hostfile + '.pem')):
         sh.openssl.genrsa('-out', '{}.key'.format(hostfile),
