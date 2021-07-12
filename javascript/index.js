5adee1a2e366bf585a825b51219f9d3cac1ae3e0
Sam Hartman <hartmans@debian.org>
2021-07-12 13:54:44 -0600

    Update license declaration in Javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,12 @@
 "use strict";
 /*
-*  Copyright (C) 2017, 2020 by Hadron Industries
-* All rights Reserved; distributed under license
+ * Copyright (C) 2017, 2020, Hadron Industries, Inc.
+ *  Entanglement is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License version 3
+ *  as published by the Free Software Foundation. It is distributed
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
+ *  LICENSE for details.
 */
 var WebSocket;
 try {
